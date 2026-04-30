8b8371b1021dd4664869cfd69d6ee2c617e49f46
camilo <alexcamflor38@gmail.com>
2026-04-30 12:48:14 -0500

    avances1

--- database/migrations/2026_04_24_161459_create_courses_table.php
+++ database/migrations/2026_04_24_161459_create_courses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('course number');
-            $table->string('day');
+            $table->date('day');
 
              $table->unsignedBigInteger('area_id')->nullable()->unique();
 
